7cd0b981b170a7ad1aed77ba5c849aff1c0758f4
Nicolas Segatto <nicolasssegatto@icloud.com>
2022-10-03 01:56:02 -0300

    update

--- src/components/UnselectVideo.tsx
+++ src/components/UnselectVideo.tsx
@@ -1,11 +1,12 @@
 import { Footer } from "./Footer";
-
+import Logo from "/src/assets/NASA_vintage.png";
 
 export function UnselectVideo(){
   return(
     <div className="flex-1 flex flex-col">
-      <div>
-        
+      <div className="flex flex-1 p-8 mx-auto justify-center items-center flex-col">
+        <img src={Logo} alt="NasaImage" className="w-96 animate-pulse" />
+        <p className="text-2xl font-bold leading-relaxed text-zinc-900">Pai vai pra <span className="text-zinc-800">NASA</span>!!! Esqueça tudo <span className="grayscale opacity-30">🚀</span></p>
       </div>
       <Footer/>
     </div>
--- src/screens/Subscribe.tsx
+++ src/screens/Subscribe.tsx
@@ -37,12 +37,12 @@ export function Subscribe() {
                 Construa uma <strong className="text-green-300">aplicação completa</strong>, do zero, com <strong className="text-green-300">React JS</strong>
               </h1>
               <p className="mt-4 text-gray-200 leading-relaxed">
-                Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+                Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas no mercado. Lembre-se, é necesario registrar presença NOS DIAS de mentoria para que a pontuação seja contabilizada.
               </p>
             </div>
 
             <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-              <strong className="text-2xl mb-6 block">Registre sua presença!</strong>
+              <strong className="text-2xl mb-6 block">Registre sua presença</strong>
 
               <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
                 <input type="text" required
